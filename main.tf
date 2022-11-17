f362c95e41f5fc80f36e256126c2fce9593ebf5d
Obusor Umesi <obusorezekiel@gmail.com>
2022-11-17 03:42:21 +0100

    modifications

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ data "aws_availability_zones" "available" {
 resource "aws_subnet" "public" {
   count                   = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index)
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
 
